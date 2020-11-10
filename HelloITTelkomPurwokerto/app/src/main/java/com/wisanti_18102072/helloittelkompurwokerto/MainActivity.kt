fb8d380ed77991ff9607d01f4f162ae3dbc42214
Wichan01 <Wisanti0104@gmail.com>
2020-11-10 21:14:12 +0700

    modul Praktikum 5

---
+++ HelloITTelkomPurwokerto/app/src/main/java/com/wisanti_18102072/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wisanti_18102072.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
